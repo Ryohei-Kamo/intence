34618f93863b75969688a52598658f96ae452e02
Ryohei-Kamo <ryohei.kamo.p4@gmail.com>
2018-10-26 16:11:29 +0900

    バインディングの修正/エラー処理の追加

--- config/class/business/getFormAction.php
+++ config/class/business/getFormAction.php
@@ -47,63 +47,90 @@ class getFormAction
 	 */
 	function setPhysicalData($data)
 	{
-		// データの保存
-		$smt = $this->pdo->prepare(
-			'INSERT INTO user_physical_dates (user_id,weight,fat_percentage,muscle_mass,water_content,visceral_fat,basal_metabolic_rate,bmi,created_at,updated_at, delete_flag) VALUES (user_id,weight,fat_percentage,muscle_mass,water_content,visceral_fat,basal_metabolic_rate,bmi,now(),now(),0)'
-		);
-		$smt->bindParam(':user_id', $data['user_id'], PDO::PARAM_STR);
-		$smt->bindParam(':weight', $data['weight'], PDO::PARAM_STR);
-		$smt->bindParam(':fat_percentage', $data['fat_percentage'], PDO::PARAM_STR);
-		$smt->bindParam(':muscle_mass', $data['muscle_mass'], PDO::PARAM_STR);
-		$smt->bindParam(':water_content', $data['water_content'], PDO::PARAM_STR);
-		$smt->bindParam(':visceral_fat', $data['visceral_fat'], PDO::PARAM_STR);
-		$smt->bindParam(':basal_metabolic_rate', $data['basal_metabolic_rate'], PDO::PARAM_STR);
-		$smt->bindParam(':bmi', $data['bmi'], PDO::PARAM_STR);
-		$smt->execute();
+		try {
+			// データの保存
+			$smt = $this->pdo->prepare(
+				'INSERT INTO user_physical_dates (user_id,weight,fat_percentage,muscle_mass,water_content,visceral_fat,basal_metabolic_rate,bmi,created_at,updated_at, delete_flag) VALUES (:user_id,:weight,:fat_percentage,:muscle_mass,:water_content,:visceral_fat,:basal_metabolic_rate,:bmi,now(),now(),0)'
+			);
+			$smt->bindParam(':user_id', $data['user_id'], PDO::PARAM_STR);
+			$smt->bindParam(':weight', $data['weight'], PDO::PARAM_STR);
+			$smt->bindParam(':fat_percentage', $data['fat_percentage'], PDO::PARAM_STR);
+			$smt->bindParam(':muscle_mass', $data['muscle_mass'], PDO::PARAM_STR);
+			$smt->bindParam(':water_content', $data['water_content'], PDO::PARAM_STR);
+			$smt->bindParam(':visceral_fat', $data['visceral_fat'], PDO::PARAM_STR);
+			$smt->bindParam(':basal_metabolic_rate', $data['basal_metabolic_rate'], PDO::PARAM_STR);
+			$smt->bindParam(':bmi', $data['bmi'], PDO::PARAM_STR);
+			$smt->execute();
+
+		} catch (PDOException $e) {
+			echo 'データの入力エラー'.$e->getMessage();
+		}
 	}
 
 	/**
 	 * データリストをDBから読み込み
 	 */
-	function getPhysicalDataList($user_id){
-		// 登録データ取得
-		$smt = $this->pdo->prepare('SELECT * FROM user_physical_dates ORDER BY created_at DESC limit 100 WHERE user_id = user_id AND delete_flag = 0');
-		$smt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-		$smt->execute();
-		// 実行結果を配列に返す。
-		$result = $smt->fetchAll(PDO::FETCH_ASSOC);
+	function getPhysicalDataList($user_id)
+	{
+		try {
+			// 登録データ取得
+			$smt = $this->pdo->prepare(
+				'SELECT * FROM user_physical_dates ORDER BY created_at DESC limit 100 WHERE user_id = :user_id AND delete_flag = 0'
+			);
+			$smt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+			$smt->execute();
+			// 実行結果を配列に返す。
+			$result = $smt->fetchAll(PDO::FETCH_ASSOC);
 
-		return $result;
+			return $result;
+
+		} catch (PDOException $e) {
+			echo 'データの読み来いエラー'.$e->getMessage();
+		}
 	}
 
 	/**
 	 * データをDBから読み込み
 	 */
-	function getPhysicalData($user_id, $data_id){
-		// 登録データ取得
-		$smt = $this->pdo->prepare('SELECT * FROM user_physical_dates WHERE user_id = user_id  AND data_id = data_id AND delete_flag = 0');
-		$smt->bindParam(':data_id', $data_id, PDO::PARAM_STR);
-		$smt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-		$smt->execute();
-		// 実行結果を配列に返す。
-		$result = $smt->fetchAll(PDO::FETCH_ASSOC);
+	function getPhysicalData($user_id, $data_id)
+	{
+		try {
+			// 登録データ取得
+			$smt = $this->pdo->prepare(
+				'SELECT * FROM user_physical_dates WHERE user_id = :user_id  AND data_id = :data_id AND delete_flag = 0'
+			);
+			$smt->bindParam(':data_id', $data_id, PDO::PARAM_STR);
+			$smt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+			$smt->execute();
+			// 実行結果を配列に返す。
+			$result = $smt->fetchAll(PDO::FETCH_ASSOC);
 
-		return $result;
+			return $result;
+		} catch (PDOException $e) {
+			echo 'データの読み込みエラー'.$e->getMessage();
+		}
 	}
 
 	/**
 	 * データを論理削除する
 	 */
-	function daletePhysicalData($user_id, $data_id){
-		// 登録データ論理削除
-		$smt = $this->pdo->prepare('UPDATE user_physical_dates SET delete_flag = 1 WHERE user_id = user_id  AND data_id = data_id');
-		$smt->bindParam(':data_id', $data_id, PDO::PARAM_STR);
-		$smt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
-		$smt->execute();
-		// 実行結果を配列に返す。
-		$result = $smt->fetchAll(PDO::FETCH_ASSOC);
+	function daletePhysicalData($user_id, $data_id)
+	{
+		try {
+			// 登録データ論理削除
+			$smt = $this->pdo->prepare(
+				'UPDATE user_physical_dates SET delete_flag = 1 WHERE user_id = :user_id  AND data_id = :data_id'
+			);
+			$smt->bindParam(':data_id', $data_id, PDO::PARAM_STR);
+			$smt->bindParam(':user_id', $user_id, PDO::PARAM_STR);
+			$smt->execute();
+			// 実行結果を配列に返す。
+			$result = $smt->fetchAll(PDO::FETCH_ASSOC);
 
-		return $result;
+			return $result;
+		} catch (PDOException $e) {
+			echo 'データの論理削除エラー'.$e->getMessage();
+		}
 	}
 
 	/**
@@ -111,18 +138,24 @@ class getFormAction
 	 */
 	function updatePhysicalData($data, $data_id)
 	{
-		// データの更新
-		$smt = $this->pdo->prepare(
-			'UPDATE  user_physical_dates SET weight = weight,fat_percentage = fat_percentage,muscle_mass = muscle_mass,water_content = water_content,visceral_fat = visceral_fat,basal_metabolic_rate = basal_metabolic_rate,bmi = bim,updated_at = now() WHERE data_id = data_id)'
-		);
-		$smt->bindParam(':data_id', $data_id, PDO::PARAM_STR);
-		$smt->bindParam(':weight', $data['weight'], PDO::PARAM_STR);
-		$smt->bindParam(':fat_percentage', $data['fat_percentage'], PDO::PARAM_STR);
-		$smt->bindParam(':muscle_mass', $data['muscle_mass'], PDO::PARAM_STR);
-		$smt->bindParam(':water_content', $data['water_content'], PDO::PARAM_STR);
-		$smt->bindParam(':visceral_fat', $data['visceral_fat'], PDO::PARAM_STR);
-		$smt->bindParam(':basal_metabolic_rate', $data['basal_metabolic_rate'], PDO::PARAM_STR);
-		$smt->bindParam(':bmi', $data['bmi'], PDO::PARAM_STR);
-		$smt->execute();
+		try {
+			// データの更新
+			$smt = $this->pdo->prepare(
+				'UPDATE  user_physical_dates SET weight = :weight,fat_percentage = :fat_percentage,muscle_mass = :muscle_mass,water_content = :water_content,visceral_fat = :visceral_fat,basal_metabolic_rate = :basal_metabolic_rate,bmi = :bim,updated_at = now() WHERE data_id = data_id)'
+			);
+			$smt->bindParam(':data_id', $data_id, PDO::PARAM_STR);
+			$smt->bindParam(':weight', $data['weight'], PDO::PARAM_STR);
+			$smt->bindParam(':fat_percentage', $data['fat_percentage'], PDO::PARAM_STR);
+			$smt->bindParam(':muscle_mass', $data['muscle_mass'], PDO::PARAM_STR);
+			$smt->bindParam(':water_content', $data['water_content'], PDO::PARAM_STR);
+			$smt->bindParam(':visceral_fat', $data['visceral_fat'], PDO::PARAM_STR);
+			$smt->bindParam(':basal_metabolic_rate', $data['basal_metabolic_rate'], PDO::PARAM_STR);
+			$smt->bindParam(':bmi', $data['bmi'], PDO::PARAM_STR);
+			$smt->execute();
+
+		} catch (PDOException $e) {
+			echo 'データの更新エラー'.$e->getMessage();
+		}
+
 	}
 }
