db6c16c2f692988f221b20ce1ab0e6f0413c767d
ryohei <ryohei@MacBookPro.local>
2018-11-11 21:27:24 +0900

    変更点を一旦プッシュ

--- config/dbProperties.php
+++ config/dbProperties.php
@@ -1,9 +1,9 @@
 <?php
 //接続データベース情報(本番)
 //記述例なので環境に応じて変更をしてください。
-define('DATABASE_NAME','physical_db');
+define('DATABASE_NAME','sport_gym');
 define('DATABASE_USER','root');
-define('DATABASE_PASSWORD','Cde3Vfr4');
+define('DATABASE_PASSWORD','triumph');
 define('DATABASE_HOST','localhost');
 
-define('PDO_DSN','mysql:dbname=' . DATABASE_NAME .';host=' . DATABASE_HOST . '; charset=utf8');
+define('PDO_DSN','mysql:host=DATABASE_HOST' . DATABASE_NAME .';host=' . DATABASE_HOST . '; charset=utf8');
